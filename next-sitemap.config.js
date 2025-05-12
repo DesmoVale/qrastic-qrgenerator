7110bb4e7642d119d91de4abaede1fcce7863820
DesmoVale <valerioleone2003.vl@gmail.com>
2025-05-12 02:37:49 +0200

    sitemap-config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://qrastic.fyi/', // cambia con il tuo dominio
+    siteUrl: 'https://qrastic.fyi', // cambia con il tuo dominio
     generateRobotsTxt: true,        // genera anche robots.txt
   }
   
